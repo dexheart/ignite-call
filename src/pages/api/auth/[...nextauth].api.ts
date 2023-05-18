8010d1f80d83934c8ad3b9294f0e98ffc20f9091
Tiago <tiago.linhares.dh@gmail.com>
2023-05-18 12:07:44 -0300

    Sixth commit

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,7 +1,7 @@
 import { PrismaAdapter } from '@/lib/auth/prisma-adapter'
 import { NextApiRequest, NextApiResponse } from 'next'
 import NextAuth, { NextAuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
 
 export function buildNextAuthOptions(
   req: NextApiRequest,
@@ -9,7 +9,7 @@ export function buildNextAuthOptions(
 ): NextAuthOptions {
   return {
     // Configure one or more authentication providers
-    adapter: PrismaAdapter(),
+    adapter: PrismaAdapter(req, res),
 
     providers: [
       GoogleProvider({
@@ -20,6 +20,15 @@ export function buildNextAuthOptions(
             scope:
               'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
           },
+        },
+        profile(profile: GoogleProfile) {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            username: '',
+            email: profile.email,
+            avatar_url: profile.picture,
+          }
         },
       }),
       // ...add more providers here
@@ -34,6 +43,13 @@ export function buildNextAuthOptions(
         }
 
         return true
+      },
+
+      async session({ session, user }) {
+        return {
+          ...session,
+          user,
+        }
       },
     },
   }
